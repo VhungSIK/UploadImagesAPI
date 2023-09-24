5a03cc458b22951f139bb659a8647740f657f379
VhungSIK <hungyn12.34n@gmail.com>
2023-09-24 10:31:14 +0700

    upload phan1

--- app/src/main/java/com/example/uploadimagesapi/MainActivity.java
+++ app/src/main/java/com/example/uploadimagesapi/MainActivity.java
@@ -1,14 +1,117 @@
 package com.example.uploadimagesapi;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.Manifest;
+
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getName();
+    private static final int MY_REQUEST_CODE = 10;
+    private EditText edtUsername, edtPassword;
+    private ImageView imgFromGallery, imgFromApi;
+    private Button btnSelecImage, btnUploadImage;
+    private TextView tvUsername, tvPassword;
+
+
+
+    private ActivityResultLauncher<Intent> mActivityReesultLaucher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Log.e(TAG, "onActivityResult");
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data == null) {
+                            return;
+                        }
+                        Uri uri = data.getData();
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                            imgFromGallery.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        //
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUi();
+
+        btnSelecImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickRequestPermisson();
+            }
+        });
     }
+    private void  initUi() {
+        edtUsername = findViewById(R.id.edt_username);
+        edtPassword = findViewById(R.id.edt_password);
+        imgFromGallery = findViewById(R.id.img_from_gallery);
+        imgFromApi = findViewById(R.id.img_from_api);
+        btnSelecImage = findViewById(R.id.btn_select_image);
+        btnUploadImage = findViewById(R.id.btn_upload_image);
+        tvUsername = findViewById(R.id.tv_username);
+        tvPassword = findViewById(R.id.tv_password);
+    }
+
+    private void onClickRequestPermisson() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            openGallery();
+            return;
+        }
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            openGallery();
+        } else {
+            String [] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permission, MY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            }
+        }
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mActivityReesultLaucher.launch(Intent.createChooser(intent, "Select Picture"));
+    }
+
 }
